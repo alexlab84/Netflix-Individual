1eb0255f536d2f6023478dfb2af02caff6106d6c
alexlab84 <alexlab84@hotmail.com>
2024-09-24 18:23:48 +0200

    Add changes in static server

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ console.log('Holis');
 
 const express = require('express'); // Importamos libreria express
 const cors = require('cors');
+const mysql = require('mysql2/promise');
+
+
 
 // create and config server
 const server = express();
@@ -15,35 +18,78 @@ server.listen(serverPort, () => {
   console.log(`Se ha iniciado el servidor en la direccion http://localhost:${serverPort}`);
 });
 
-server.get('/movies', (req, res) => {
- 
+
+async function getConnection () {
+  try {
+
+    const conn = await mysql.createConnection({
+      host: 'localhost',
+      port: 3306,
+      user:'root',
+      password:'Tacirupeca84',
+      database:'netflixpruebasale'
+    })
   
-  const fakeMovies = [
-    {
-      id: 1,
-      title: "Wonder Woman",
-      genre: "Action",
-      image:
-        "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
-      category: "Superhero",
-      year: 2017,
-      director: "Patty Jenkins",
-    },
-    {
-      id: 2,
-      title: "Inception",
-      genre: "Science Fiction",
-      image:
-        "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
-      category: "Thriller",
-      year: 2010,
-      director: "Christopher Nolan",
-    },
-    
-  ];
+  await conn.connect();
+  
+  return conn;
+  
+    }
+    catch(error) {
+      console.log(error);
+        res.status(500).send('Se rompio');
+    }
+}
+
+
+
+server.get('/movies', async (req, res) => {
+
+ const conn = await getConnection();
+
+  if (!conn) {
+    res.status(500).send('Se rompio');
+    return;
+  }
+
+  console.log(req.query);
+
+  const genreFilterParam = req.query.genre;
+
+  let query = 'SELECT * FROM movies';
+  const queryParams = [];
+
+  // Si existe el parámetro de género, filtramos por género
+  if (genreFilterParam) {
+    query += ' WHERE genre = ?'; // Agregamos el filtro por género
+    queryParams.push(genreFilterParam); // Agregamos el valor del parámetro
+  }
+
+  
+  
+  const [results, columns] = await conn.query('SELECT * FROM movies;');
+
+  // const results = array[0];
+
+  // const [results, columns] = array;
+
+  console.log(results);
+  
+
   res.json({
     success: true,
-    movies:  fakeMovies
-  })
+    movies:  results
+  });
+
+  conn.close();
+  
+
   
 });
+
+
+server.use(express.static('src/public-react'));
+
+server.use(express.static('src/public-movies-images'));
+
+
--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -1,11 +1,15 @@
 // login
 
-const getMoviesFromApi = () => {
+const getMoviesFromApi = (params) => {
+
+  console.log(params); 
   
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
   const movies = 'movies';
 
-  return fetch(`http://localhost:4000/${movies}`)
+  const genreQuery = params.genre ? `?genre=${params.genre}` : '';
+
+  return fetch(`http://localhost:4000/${movies}${genreQuery}`)
 
     .then(response => response.json())
     .then(data => {
